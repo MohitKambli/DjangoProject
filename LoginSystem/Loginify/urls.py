9a39eed7bd2b0433b46f7e992853457b5e0213c4
Mohit Kambli <mohitkambli8@gmail.com>
2025-03-12 04:10:43 -0400

    Implementing CRUD APIs for Task-5

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import hello_world, signup, login
+from .views import hello_world, signup, login, get_all_users, get_user_by_email, update_user, delete_user
 
 urlpatterns = [
-    path('hello/', hello_world),
+    path("hello/", hello_world),
     path("signup/", signup, name = 'signup'),
     path("login/", login, name = 'login'),
+    path("users/", get_all_users, name="get_all_users"),
+    path("users/<str:email>", get_user_by_email, name="get_user_by_email"),
+    path("users/update/<str:email>", update_user, name="update_user"),
+    path("users/delete/<str:email>", delete_user, name="delete_user"),
 ]
--- LoginSystem/Loginify/views.py
+++ LoginSystem/Loginify/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import UserDetails
+from .serializers import UserDetailsSerializer
+import json
 
 # Create your views here.
 def hello_world(request):
@@ -25,10 +27,86 @@ def login(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        
         try:
             user = UserDetails.objects.get(username = username, password = password)
             return render(request, "success.html", {"user": user})
         except Exception as e:
             return HttpResponse("Invalid credentials!")
     return render(request, "login.html")
+
+@csrf_exempt
+def get_all_users(request):
+    if request.method == "GET":
+        users = UserDetails.objects.all()
+        serializer_data = UserDetailsSerializer(users, many = True)
+        return JsonResponse(serializer_data.data,safe=False)
+
+@csrf_exempt
+def get_user_by_email(request, email):
+    if request.method == "GET":
+        try:
+            user = UserDetails.objects.get(email = email)
+            serializer_data = UserDetailsSerializer(user)
+            return JsonResponse(
+                {
+                    "success":True,
+                    "Data":serializer_data.data
+                }, 
+                status = 200
+            )
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "success":False,
+                    "error":str(e)
+                }, 
+                status = 400
+            )
+
+@csrf_exempt
+def update_user(request, email):
+    if request.method in ["PUT", "PATCH"]:
+        try:
+            user = UserDetails.objects.get(email = email)
+            input_data = json.loads(request.body)
+            serializer_data = UserDetailsSerializer(user, data=input_data, partial=(request.method == "PATCH"))
+            if serializer_data.is_valid():
+                serializer_data.save()
+                return JsonResponse(
+                    {
+                        "Success":True,
+                        "message":"Data Updated Successfully",
+                        "Updated Data ": serializer_data.data
+                    }, 
+                    status = 200
+                )
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "success":False,
+                    "Error":str(e)
+                }, 
+                status = 400
+            )
+
+@csrf_exempt
+def delete_user(request, email):
+    if request.method == 'DELETE':
+        try:
+            user = UserDetails.objects.get(email = email)
+            user.delete()
+            return JsonResponse(
+                {
+                    "Success":True,
+                    "message":"Data Deleted Successfully",
+                }, 
+                status = 200
+            )
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "success":False,
+                    "Error":str(e)
+                }, 
+                status = 400
+            )
